ccb210ea2292cb6484b5178a2db5826eceeff6cb
JasonTrapp12 <jwt9552@g.rit.edu>
2023-11-02 14:55:30 -0400

    added modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,18 @@
 import React from "react";
+import ParkingLotModal from "./ParkingLotModal";
 
 const Card = ({ image, name }) => {
+    const displayDetails = () => {
+        console.log({name});
+    }
+
     return(
-        <div className="card">
+        <div className="card" onClick={ParkingLotModal.handleShow}>
         <img src={image}></img>
-        <p> {name} </p>
+        <div className="name-and-openbutton">
+            <h2> {name} </h2>
+            <ParkingLotModal />
+        </div>
         </div>
     )
 
