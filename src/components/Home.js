495d2fd36019db00cabe80378bf07c24c2c62841
JasonTrapp12 <jwt9552@g.rit.edu>
2023-11-02 13:15:26 -0400

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="title">
+                RIT Parking
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
