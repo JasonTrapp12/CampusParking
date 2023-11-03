1329ca096d55dae3938578a19f2cbd5b63b33915
JasonTrapp12 <jwt9552@g.rit.edu>
2023-11-02 23:09:31 -0400

    got rid of some warnings

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,10 @@ import React from "react";
 import ParkingLotModal from "./ParkingLotModal";
 
 const Card = ({ image, name, description }) => {
-    const displayDetails = () => {
-        console.log({name});
-    }
 
     return(
         <div className="card">
-        <img src={image}></img>
+        <img src={image} alt=""></img>
         <div className="name-and-openbutton">
             <h2> {name} </h2>
             <ParkingLotModal image={image} name={name} description={description}/>
