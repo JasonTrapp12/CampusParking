02677cc7a853d401ade68ea8d8f2c2f14ce79fee
JasonTrapp12 <jwt9552@g.rit.edu>
2023-11-03 14:58:13 -0400

    added descriptions for lots

--- src/components/Lots.js
+++ src/components/Lots.js
@@ -4,103 +4,103 @@ export const lotsData = [
         id : 1,
         name : "D Lot",
         image : "LotImages/DLot.jpg",
-        description : "D-Lot is a medium-sized parking lot located next to Gordon Field House and the Welcome Center.  This lot is a short walk to many of the buildings located between Academic and Dorm sides, but its often full throughout the day.  The following types of parking are available: Handicapped, Reserved, General, Motorcycle, Electric Charging"
+        description : "D-Lot is a medium-sized parking lot located next to Gordon Field House and the Welcome Center.  This lot is a short walk to many of the buildings located between Academic and Dorm sides, but is often full throughout the day.  The following types of parking are available: Handicapped, Reserved, General, Motorcycle, Electric Charging"
         
     },
     {
         id : 2,
         name : "N Lot",
         image : "LotImages/NLot.jpg",
-        description : "good parking"
+        description : "N-Lot is a small overflow lot for D Lot and G-Lot.  Its fully general and further away from all buildings than the adjacent lots."
     },
     {
         id : 3,
         name : "H Lot",
         image : "LotImages/HLot.jpg",
-        description : "H lot parking"
+        description : "H-Lot is a large general overflow lot for F-Lot and is connected to G-Lot. Its a far walk, but this lot is generally meant for accessing Institute Hall, Engineering Hall, Orange Hall, Gosnell Hall, Booth Hall, and Liberal Arts Hall."
     },
     {
         id : 4,
         name : "G Lot",
         image : "LotImages/GLot.jpg",
-        description : "G lot parking"
+        description : "G-Lot is a small overflow lot for E-Lot and is connected to H-Lot.  Its a far walk, but this lot is generally meant for accessing Institute Hall, Engineering Hall, Orange Hall, Gosnell Hall, Booth Hall, and Liberal Arts Hall.  The following types of parking are available: General, Handicapped"
     },
     {
         id : 5,
         name : "E Lot",
         image : "LotImages/ELot.jpg",
-        description : "E lot parking"
+        description : "E-Lot is a small lot right out front of Magic, with close access to Institute Hall, Engineering Hall, Orange Hall, Gosnell Hall, Booth Hall, Gannett Hall, Wallace Library, and Liberal Arts Hall.  The following types of parking are available: Admin, Handicapped, Reserved"
     },
     {
         id : 6,
         name : "F Lot",
         image : "LotImages/ELot.jpg",
-        description : "E lot parking"
+        description : "F-Lot is a medium-sized lot located right next to Institute Hall, with close access to Engineering Hall, Orange Hall, Gosnell Hall, Booth Hall, Gannett Hall, Wallace Library, and Liberal Arts Hall.  The following types of parking are available: Reserved, General, Handicapped, Motorcycle"
     },
     {
         id : 7,
         name : "J Lot",
         image : "LotImages/JLot.jpg",
-        description : "J lot parking"
+        description : "J-Lot is a medium-sized lot located right next to Golisano and Institute Hall.  Its also a close option for Eningineering Hall and Gleason Hall.  The following types of parking are available: Handicapped, Admin, Reserved, General, Motorcycle"
     },
     {
         id : 8,
         name : "T Lot",
         image : "LotImages/TLot.jpg",
-        description : "T lot parking"
+        description : "T-Lot is a small lot located right next to Slaughter Hall and is a short distance from Golisano.  The following types of parking are avaialable: Handicapped, Admin, General, Electric Vehicle"
     },
     {
         id : 9,
         name : "S Lot",
         image : "LotImages/SLot.jpg",
-        description : "S lot parking"
+        description : "S-lot is a large lot located next to Global Village.  The following types of parking are available: Handicapped, Admin, Reserved, General, Motorcycle"
     },
     {
         id : 10,
         name : "R Lot",
         image : "LotImages/RLot.jpg",
-        description : "R lot parking"
+        description : "R-lot is a small lot located next to S-Lot and Brown Hall.  The following types of parking are available: Handicapped & Reserved"
     },
     {
         id : 11,
         name : "U Lot",
         image : "LotImages/ULot.jpg",
-        description : "U lot parking"
+        description : "U-lot is a small lot located next to the Gene Polisseni Center and Community Gardens.  The following types of parking are available: Handicapped & Reserved"
     },
     {
         id : 12,
         name : "C Lot",
         image : "LotImages/CLot.jpg",
-        description : "C lot parking"
+        description : "C-lot is a medium-sized lot located next to Kate Gleason Hall and Gracie's.  The following types of parking are available: General & Vendor"
     },
     {
         id : 13,
         name : "K Lot",
         image : "LotImages/KLot.jpg",
-        description : "K lot parking"
+        description : "K-Lot is a large parking lot located outside of Sol Hall.  This lot it predominantly used for dorm parking.  The following types of parking are available: General"
     },
     {
         id : 14,
         name : "B Lot",
         image : "LotImages/BLot.jpg",
-        description : "B lot parking"
+        description : "B-Lot is a large parking lot located near dorms on Wiltsie Dr.  This lot it predominantly used for dorm parking.  The following types of parking are available:General & Resident"
     },
     {
         id : 15,
         name : "A Lot",
         image : "LotImages/ALot.jpg",
-        description : "A lot parking"
+        description : "A-Lot is a small parking lot located near dorms on Wiltsie Dr.  This lot it predominantly used for dorm parking.  The following types of parking are available: General"
     },
     {
         id : 16,
         name : "L Lot",
         image : "LotImages/LLot.jpg",
-        description : "L lot parking"
+        description : "L-lot is a medium-sized parking lot located next to Lyndon Baines Johnson Hall. The following types of parking are available: Handicapped, Admind, General, Motorcycle"
     },
     {
         id : 17,
         name : "M Lot",
         image : "LotImages/MLot.jpg",
-        description : "M lot parking"
+        description : "M-lot is a small parking lot located next to Rosica Hall and The Commons.  The following types of parking are available: Handicapped, Admin, Reserved, Electric Vehicle, Vendor"
     },
 ]
